9bb4f550a4ce00297bde94f9458a5a95fe6b1b6b
@batyr.ashim2022 <batyr.ashim2022@gmail.com>
2024-06-18 12:00:20 +0500

    Batyr new

---
+++ src/components/PropsComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ company }}</div>
+</template>
+
+<script setup>
+const { props } = defineProps({
+  company: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style></style>
--- src/pages/Props.vue
+++ src/pages/Props.vue
@@ -2,16 +2,14 @@
 <template>
   <div>
     <h4>Здесь мы практикуем Props в Vue js</h4>
-
+    <PropsComponent :company="company" />
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import PropsComponent from "../components/PropsComponent.vue";
 
-}
+const company = ["Google", "Facebook"];
 </script>
 
-<style>
-
-</style>
+<style></style>
