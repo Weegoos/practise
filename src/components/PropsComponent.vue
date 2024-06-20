e31f801bfb3b3f65e342319be73f6205631d6799
Weegoos <batyr.ashim05@mail.ru>
2024-06-20 22:48:41 +0500

    Batyr new

--- src/components/PropsComponent.vue
+++ src/components/PropsComponent.vue
@@ -1,36 +1,19 @@
 <template>
-<<<<<<< HEAD
   <div>
-    <i>Дочерний компонент PropsComponent.vue</i> <br>
-    <code><i>{{ message }}</i></code>
+    <i>Дочерний компонент PropsComponent.vue</i> <br />
+    <code
+      ><i>{{ message }}</i></code
+    >
   </div>
 </template>
 
 <script setup>
-
-const {props} = defineProps({
-    message: {
-        type: String,
-        required: true
-    }
-})
-</script>
-
-<style>
-
-</style>
-=======
-  <div>{{ company }}</div>
-</template>
-
-<script setup>
 const { props } = defineProps({
-  company: {
-    type: Array,
+  message: {
+    type: String,
     required: true,
   },
 });
 </script>
 
 <style></style>
->>>>>>> 93e3e6f2867c5a35d98ca370dc85cef620ebda9b
